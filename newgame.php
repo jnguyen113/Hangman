303fdaedaa3818302d312a351158f35b87bcd2fe
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2021-03-22 02:22:21 -0400

    feat: setup new game page
    - present categories and then pick a word from it

--- newgame.php
+++ newgame.php
@@ -14,7 +14,83 @@ session_start();
 require_once('hangman.php');
 require_once('util.php');
 
-hangman_start();
+/* reads categories to then read words */
+function read_categories()
+{
+  $categories = array();
+  $fd = fopen("words/categories.txt", "r");
 
-header("Location: play.php");
-exit();
+  while (!feof($fd)) {
+    $result = trim(fgets($fd));
+    array_push($categories, $result);
+  }
+  fclose($fd);
+  return $categories;
+}
+
+function read_words($categories)
+{
+  $words = array();
+  foreach ($categories as $category) {
+    // Create filename
+    $filename = str_replace(" ", "_", strtolower($category)) . ".txt";
+
+    // Read file contents to array
+    $temp = array();
+    $fd = fopen("words/" . $filename, "r");
+    if ($fd)
+      while (!feof($fd)) {
+        $result = strtoupper(trim(fgets($fd)));
+        array_push($temp, $result);
+      }
+    fclose($fd);
+
+    $words[strtolower($category)] = $temp;
+  }
+  return $words;
+}
+
+/* Determine if we are starting a new game or we have started a new game */
+$categories = read_categories();
+$words = read_words($categories);
+
+if (isset($_REQUEST['category'])) {
+  $cat = strtolower($_REQUEST['category']);
+  $rand_key = array_rand($words[$cat], 1);
+  $selected_word = $words[$cat][$rand_key];
+  hangman_start($selected_word, $cat);
+  header("Location: play.php");
+  exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Hangman | New Game</title>
+  <link href="play.css<?php echo '?' . rs(7); ?>" rel="stylesheet">
+</head>
+
+<body>
+  <div class="main future">
+    <div class="category_header center">
+      <h1>Categories</h1>
+    </div>
+    <form method="post" class="box">
+      <?php
+      $fd = fopen("words/categories.txt", "r");
+
+      while (!feof($fd)) {
+        $result = trim(fgets($fd));
+        echo "<button class=\"center future\" type=\"submit\" name=\"category\" value=\"" . $result . "\"><h2>" . $result . "</h2></button>\n";
+      }
+      fclose($fd);
+      echo "</form>";
+      ?>
+    </div>
+  </div>
+</body>
+
+</html>
